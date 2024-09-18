e3cee9147bebc1405644344c8b0ca8be54ab9d33
KoniChivaJS <mredik200@gmail.com>
2024-09-18 20:35:34 +0300

    JV-3_4

--- java_3/java-3_3/src/Main.java
+++ java_3/java-3_3/src/Main.java
@@ -1,4 +1,15 @@
 public class Main {
+
+    public static <T extends Comparable<T>> T findMax(T[] arr){
+        T max = arr[0];
+        for(T element : arr){
+            if(element.compareTo(max) > 0) {
+                max = element;
+            }
+        }
+        return max;
+    }
+
     public static void main(String[] args) {
         Box<String> stringBox = new Box<>();
         stringBox.putItem("Hello Wordl!");
@@ -9,6 +20,14 @@ public class Main {
         Box<Integer> integerBox = new Box<>();
         integerBox.putItem(911);
         System.out.println("Integer box: " + integerBox.getItem());
+
+
+        System.out.println("TASK 4");
+        Integer[] intArr = {1,5,777,3,2};
+        System.out.println("Max int: " + findMax(intArr));
+
+        Double[] doubleArr = {1.2,9.0,55.2,86.1};
+        System.out.println("Max double: " + findMax(doubleArr));
 
     }
 }
